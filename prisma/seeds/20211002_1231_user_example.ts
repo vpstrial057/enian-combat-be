d8d5a8155891fe57c75c2c13f7a681c51d584fb2
Habibie <mrhabibiedev@gmail.com>
2024-10-07 13:24:24 +0700

    update user seeder

--- prisma/seeds/20211002_1231_user_example.ts
+++ prisma/seeds/20211002_1231_user_example.ts
@@ -1,22 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker'
 
 const prisma = new PrismaClient();
 
 export async function run(): Promise<void> {
-  const users = generateUsers(10);
-  
-  for (const user of users) {
-    try {
-      await prisma.user.create({ data: user });
-      console.log(`Created user with wallet address: ${user.walletAddress}`);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error(`Failed to create user with wallet address: ${user.walletAddress}, Error: ${error.message}`);
-      } else {
-        console.error(`Failed to create user with wallet address: ${user.walletAddress}, Unknown error occurred`);
-      }
-    }
-  }
+  await prisma.user.createMany({ data: generateUsers(5) });
+}
+
+export async function clearUsers(): Promise<void> {
+  await prisma.user.deleteMany()
 }
 
 function generateUsers(count: number): Array<{
@@ -29,19 +21,35 @@ function generateUsers(count: number): Array<{
   updatedBy: string;
 }> {
   const users = [];
-  
+
   for (let index = 0; index < count; index++) {
     const timestamp = Date.now();
     users.push({
-      walletAddress: `0x${timestamp}${index}`,
-      telegramId: `telegram${index}`,
-      tonWallet: `ton${timestamp}${index}`,
-      evmAddress: `evm${timestamp}${index}`,
+      walletAddress: faker.string.hexadecimal({ length: 16, prefix: '0x' }),
+      telegramId: faker.string.numeric(10),
+      tonWallet: faker.string.hexadecimal({ length: 16, prefix: 'TON', casing: 'upper' }),
+      evmAddress: faker.string.hexadecimal({ length: 16, prefix: '0x' }),
       gold: Math.floor(Math.random() * 1000),
       createdBy: 'Seeder',
       updatedBy: 'Seeder',
     });
   }
-  
+
   return users;
 }
+
+async function seed(): Promise<void> {
+  await prisma.mission.deleteMany()
+  await prisma.user.deleteMany()
+  // await clearUsers()
+  await run()
+}
+
+seed().catch((error: unknown) => {
+  if (error instanceof Error) {
+    console.error(error.message);
+  } else {
+    console.error('Unknown error occurred during seeding');
+  }
+  process.exit(1);
+});
