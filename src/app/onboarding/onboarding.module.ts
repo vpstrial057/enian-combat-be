4482420602a60309b3ef97e37be20d4f35c5b35e
Habibie <mrhabibiedev@gmail.com>
2024-10-22 20:56:40 +0700

    implement auth get onboarding

--- src/app/onboarding/onboarding.module.ts
+++ src/app/onboarding/onboarding.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OnboardingService } from './onboarding.service';
 import { OnboardingController } from './onboarding.controller';
+import { UserService } from '../user/user.service';
 
 @Module({
   controllers: [OnboardingController],
-  providers: [OnboardingService],
+  providers: [OnboardingService, UserService],
 })
 export class OnboardingModule {}
